5a03a87de5015d0f8d9446c3344cda13e0ed5e09
Fabio Ivona <fabio.ivona@defstudio.it>
2021-10-05 11:26:34 +0200

    adds mock_authorization pest helper

--- src/Pest/helpers.php
+++ src/Pest/helpers.php
@@ -1,5 +1,23 @@
 <?php
 
+
+use Illuminate\Auth\Access\AuthorizationException;
+
+function mock_authorization(string $action, mixed $target = null, $allow = true): void
+{
+    if ($allow) {
+        Gate::shouldReceive('authorize')
+            ->with($action, $target)
+            ->andReturn(true)
+            ->atLeast()->once();
+    } else {
+        Gate::shouldReceive('authorize')
+            ->with($action, $target)
+            ->andThrow(AuthorizationException::class)
+            ->atLeast()->once();
+    }
+}
+
 function bind_mock(string $class, callable ...$methods)
 {
     $mock = mock($class)->expect(...$methods);
