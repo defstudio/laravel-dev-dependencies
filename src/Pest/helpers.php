d4e46ca0334f0e87ffa9936275dfa91875bc1019
Fabio Ivona <fabio.ivona@defstudio.it>
2022-02-09 13:55:03 +0100

    Update helpers.php

--- src/Pest/helpers.php
+++ src/Pest/helpers.php
@@ -23,3 +23,42 @@ function bind_mock(string $class, callable ...$methods)
     $mock = mock($class)->expect(...$methods);
     app()->bind($class, fn () => $mock);
 }
+
+function bind_test_double(object $class)
+{
+    app()->bind(get_parent_class($class), $class::class);
+}
+
+/**
+ * @param class-string $model_class
+ */
+function mock_model(string $model_class)
+{
+    $model_class = eval(<<<CLASS
+        return new class() extends $model_class {
+            public bool \$has_been_saved = false;
+            public bool \$has_been_deleted = false;
+
+            public function save(array \$options = []): bool
+            {
+                \$this->exists = true;
+                \$this->has_been_saved = true;
+                return true;
+            }
+
+            public function delete(): bool|null
+            {
+                \$this->exists = false;
+                \$this->has_been_deleted = true;
+                return true;
+            }
+        };
+    CLASS);
+
+    mock_class($model_class);
+}
+
+function mock_class(object $class)
+{
+    bind_test_double($class);
+}
