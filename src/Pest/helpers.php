383778152d6b821c43dd8a55ed6b85b78fd5ed26
Fabio Ivona <fabio.ivona@defstudio.it>
2022-02-10 10:21:31 +0100

    Update helpers.php

--- src/Pest/helpers.php
+++ src/Pest/helpers.php
@@ -33,49 +33,6 @@ function bind_mock(string $class, callable ...$methods): object
     return $mock;
 }
 
-/**
- * @template TClass
- * @param class-string<TClass> $class
- * @param callable ...$methods
- *
- * @return TClass|\Mockery\MockInterface
- */
-function partial_mock(string $class, callable ...$methods): object{
-    return bind_mock($class, ...$methods)->makePartial();
-}
-
-/**
- * @template TClass
- *
- * @param class-string<TClass> $model_class
- *
- * @return TClass
- */
-function mock_model(string $model_class)
-{
-    $model_class = eval(<<<CLASS
-        return new class() extends $model_class {
-            public bool \$has_been_saved = false;
-            public bool \$has_been_deleted = false;
-
-            public function save(array \$options = []): bool
-            {
-                \$this->exists = true;
-                \$this->has_been_saved = true;
-                return true;
-            }
-
-            public function delete(): bool|null
-            {
-                \$this->exists = false;
-                \$this->has_been_deleted = true;
-                return true;
-            }
-        };
-    CLASS);
-
-    return bind_test_double($model_class);
-}
 
 /**
  * @param object $class
